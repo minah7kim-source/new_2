77f7872773551499979b307d3af5adedc7313056
minah7kim-source <minah7kim@gmail.com>
2026-06-23 12:53:28 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import os
 import streamlit as st
 import pandas as pd
-import plotly.express as px
 from collections import Counter
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
